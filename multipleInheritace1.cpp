23568aba9e427cf6d5ed0ed4cf1c157c4cabc0b7
Rafi <smrafi120@gmail.com>
2022-12-10 09:47:39 +0600

    first commit

--- multipleInheritace1.cpp
+++ multipleInheritace1.cpp
@@ -1,23 +1,33 @@
 #include<iostream>
 using namespace std;
-class A{
+class Base1{
+	protected:
+		int base1int;
 	public:
-		int x,y;
-		public:
-			void sum(){
-				int sum=0;
-				sum=x+y;
-			}
-};
-class B:public A{
-	public:
-	void display(){
-			cout<<sum;
+		void set_base1int(int x){
+				base1int=x;
 		}
-	
+};
+class Base2{
+	protected:
+		int base2int;
+	public:
+		void set_base2int(int x){
+				base2int=x;
+		}
+};
+class Derived:public Base1,public Base2{
+	public:
+		void show(){
+			cout<<"The value of base1 is "<<base1int<<endl;
+			cout<<"The value of base2 is "<<base2int<<endl;
+			cout<<"The sum of these valudes = "<<base1int+base2int<<endl;
+		}
 };
 int main(){
-	B obj;
-    obj.display();
+	Derived obj;
+	obj.set_base1int(10);
+	obj.set_base2int(10);
+	obj.show();
 	return 0;
 }
